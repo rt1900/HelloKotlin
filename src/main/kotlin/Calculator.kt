33cf4b37e667501dea2241894d8872d30c1a34ee
rasul aslanov <rasul.aslanov@metro.digital>
2024-02-22 07:38:09 +0100

    Task 1.2 and 1.3

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,27 @@
+fun main(){
+
+println("Enter <<calc>> if you want to calculate numbers")
+val userInput = readlnOrNull()
+    if (userInput == "calc")
+    {print(calculator())}
+    else{println("If you want to calculate numbers, run the program again and write <<calc>>")}
+}
+fun calculator()
+{    print ("Please select an operation by entering the appropriate number:\n 1 for Addition\n 2 for Subtraction\n 3 for Multiplication\n 4 for Division\n Please enter number: ")
+    val operationNumber = readln().toInt()
+    println ("Enter the first Number:")
+    val firstnumber = readln().toInt()
+    println ("Enter the second Number:")
+    val secondnumber = readln().toInt()
+
+    if (operationNumber == 1)
+    {println("Result: $firstnumber + $secondnumber = " + (firstnumber + secondnumber))}
+    else if (operationNumber == 2)
+    {println("Result: $firstnumber - $secondnumber = " + (firstnumber - secondnumber))}
+    else if (operationNumber == 3)
+    {println("Result: $firstnumber * $secondnumber = " + (firstnumber * secondnumber))}
+    else if (operationNumber == 4)
+    {println("Result: $firstnumber / $secondnumber = " + (firstnumber / secondnumber))}
+    else{println("you chose the wrong operator number")}
+}
+
---
+++ src/main/kotlin/Greetings.kt
@@ -0,0 +1,15 @@
+@file:JvmName("GreetingsKt")
+
+fun main() {
+   println("Enter text: ")
+    val enterString = readln()
+    if (enterString == "Hello" || enterString =="hello")
+    {println(userInput()) }
+    else
+    {println ("Thx && goodbye")}
+   }
+fun userInput()
+{
+    println("Hello Julian")
+}
+
