b288141671db54d3f35e41fc0b335106b05d2a29
rasul aslanov <rasul.aslanov@metro.digital>
2024-02-27 09:18:40 +0100

    Rasul|Task 2.2|Added a program that prints numbers from an array

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -5,12 +5,29 @@ numbers and one operation from the user and give an answer */
 
 fun main(){
 
-println("Enter <<calc>> if you want to calculate numbers")
+println("""Press the corresponding number to select the program
+    |1 for Calculator
+    |2 for Array of numbers
+""".trimMargin())
+
 val userInput = readlnOrNull()
-    if (userInput == "calc")
-    {print(calculator())}
-    else{println("If you want to calculate numbers, run the program again and write <<calc>>")}
+    if (userInput == "1")
+       {print(calculator())}
+    else if(userInput == "2")
+       {println(array())}
+    else{println("Яou chose the wrong number")}
 }
+
+
+fun array() {
+
+    val numbers = arrayOf(1, 2, 3, 4)
+
+    for (x in numbers) {
+        println(x)
+    }
+}
+
 fun calculator()
 {    print ("Please select an operation by entering the appropriate number:\n 1 for Addition\n 2 for Subtraction\n 3 for Multiplication\n 4 for Division\n Please enter number: ")
     val operationNumber = readln().toInt()
